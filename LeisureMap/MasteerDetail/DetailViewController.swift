87cc4bb917021f072cc627c8706333ef022f679c
zyouking <zyouqing@qq.com>
2018-07-25 16:20:56 +0800

    no message

---
+++ LeisureMap/MasteerDetail/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  LeisureMap
+//
+//  Created by stu1 on 2018/7/25.
+//  Copyright © 2018年 stu1. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
