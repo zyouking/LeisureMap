1fa739a8a423bcc334a768a24b47908f8aac5c8f
zyouking <zyouqing@qq.com>
2018-07-26 09:53:14 +0800

    test version

--- LeisureMap/SplashViewController.swift
+++ LeisureMap/SplashViewController.swift
@@ -12,6 +12,8 @@ class SplashViewController: UIViewController {
 
     // score.azurewebsites.net/api/version/1
     
+    var appVersion:String=""
+    @IBOutlet weak var lbVersion: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,29 @@ class SplashViewController: UIViewController {
         
         let defaults:UserDefaults=UserDefaults.standard
         defaults.synchronize()
+        
+        appVersion=""+(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)!
+        lbVersion.text=appVersion
+        
+        
+        let from="https://score.azurewebsites.net/api/version/\(String(describing:appVersion))"
+        let url=URL(string:from)!
+        let requst=URLRequest(url: url)
+        let config=URLSessionConfiguration.default
+        let session=URLSession(configuration:config)
+        let task=session.dataTask(with: requst, completionHandler: {(data,response,error) in
+            let httpResponse=response as! HTTPURLResponse
+            let statusCode=httpResponse.statusCode
+            if(200==statusCode){
+                let dataString=NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                let responseString=String(dataString!)
+//                print(<#T##items: Any...##Any#>, separator: <#T##String#>, terminator: <#T##String#>)
+                print(responseString)
+            }
+            
+            
+        })
+        task.resume()
     }
     
 
